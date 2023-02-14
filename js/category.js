d57b8a2ddc421ebe44c7626f6d32096757500bce
majamariaaaby <majamariaaaby@hotmail.com>
2023-02-14 12:22:59 +0100

    first commit

---
+++ js/category.js
@@ -0,0 +1,19 @@
+fetch("https://kea-alt-del.dk/t7/api/categories")
+  .then((res) => res.json())
+  .then(showCategories);
+
+function showCategories(cats) {
+  cats.forEach(showCategory);
+}
+
+function showCategory(cat) {
+  //fanger vores template
+  const template = document.querySelector("template").content;
+  //cloner
+  const clone = template.cloneNode(true);
+  //ændrer indhold
+  clone.querySelector("a").textContent = cat.category;
+  clone.querySelector("a").href = `productlist.html?category=${cat.category}`;
+  //appender
+  document.querySelector(".grid_1-1 ol").appendChild(clone);
+}
