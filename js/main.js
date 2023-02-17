9c1c1d955a3b5dd21a4fbd65cc83d9b8018171c6
Esther B <esth3734@stud.kea.dk>
2023-02-17 09:21:52 +0100

    js oprettet

--- js/main.js
+++ js/main.js
@@ -7,7 +7,22 @@ fetch("https://litteratur-ba09.restdb.io/rest/books", {
   .then((e) => e.json())
   .then(doSomething);
 
-function doSomething(data) {
-  data.forEach((element) => {});
-  console.log(data);
+function doSomething(books) {
+  //fang template
+  const template = document.querySelector("#smallProductTemplate").content;
+
+  books.forEach((book) => {
+    // console.log("book");
+
+    //lav en kopi
+    const copy = template.cloneNode(true);
+    //ændre indhold
+
+    copy.querySelector("h2").textContent = book.title;
+    copy.querySelector("#author").textContent = book.author;
+    copy.querySelector("#price").textContent = book.price + ".- kr";
+    copy.querySelector("img").src = book.image;
+    // copy.querySelector("a").href = `product.html?id=${book.id}`;
+    document.querySelector(".grid_1-1-1").appendChild(copy);
+  });
 }
